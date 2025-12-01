4ef73f56a5703d373b4a68148af092e54313aada
strophiccat <strophiccat@gmail.com>
2025-12-01 15:49:34 +0200

    test fix

--- src/Sanctuary.WebAPI/Program.cs
+++ src/Sanctuary.WebAPI/Program.cs
@@ -12,8 +12,6 @@ using Sanctuary.Database;
 using Sanctuary.WebAPI.Endpoints;
 using Sanctuary.WebAPI.Options;
 
-CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 var builder = WebApplication.CreateBuilder(args);
 
